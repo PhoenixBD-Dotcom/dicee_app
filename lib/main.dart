43a45a4d39da867c0e152854fb64cbb0b621359b
Md.Sakibullah <sakibullah98@gmail.com>
2024-04-20 22:31:35 +0600

    updated dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,75 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(
+            child: Text(
+              'Dicee',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: SafeArea(
+          child: DicePage(),
+        ),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftdicenum = 1;
+  var rightdicenum = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftdicenum = Random().nextInt(6)+1;
+                  });
+                },
+                child: Image.asset('images/dice$leftdicenum.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    rightdicenum = Random().nextInt(6)+1;
+                  });
+                },
+                child: Image.asset('images/dice$rightdicenum.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
